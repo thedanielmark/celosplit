a04d99316593531ce8417815cfba9123fa8671fe
Daniel Mark <danielmark.uc@gmail.com>
2024-07-14 03:45:26 +0200

    Contract details util

---
+++ fe/utilities/contractDetails.ts
@@ -0,0 +1,15 @@
+import CeloSplitContract from "@/abis/CeloSplit.json";
+import CSTokenContract from "@/abis/CSToken.json";
+
+// type Address = `0x${string}`;
+
+export const CONTRACT_ABIS = {
+  CeloSplitContract,
+  CSTokenContract,
+};
+
+// export const SPLIT_MONEY_CONTRACT_ADDRESS: Address =
+//   "0xB03fc565F3A0b7Ab884E3f4C1fC6b5CFCd440081";
+
+// export const SM_TOKEN_CONTRACT_ADDRESS: Address =
+//   "0x71050CDD8df15ac474E58002373Ad12aa3B5425a";
