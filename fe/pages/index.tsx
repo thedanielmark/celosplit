cd3818d618af43fc28f53aa924ef099f272917ce
Daniel Mark <danielmark.uc@gmail.com>
2024-07-14 03:55:01 +0200

    New homepage

--- fe/pages/index.tsx
+++ fe/pages/index.tsx
@@ -1,118 +1,144 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import React from "react";
+import Head from "next/head";
+import {
+  ArrowsRightLeftIcon,
+  BanknotesIcon,
+  BellAlertIcon,
+  ClockIcon,
+  CreditCardIcon,
+} from "@heroicons/react/24/outline";
+import Link from "next/link";
+import { DocumentDuplicateIcon } from "@heroicons/react/24/solid";
+import { useAccount } from "wagmi";
+import { superShortenAddress } from "@/utilities/shortenAddress";
 
-const inter = Inter({ subsets: ["latin"] });
+export default function Index() {
+  const { address } = useAccount();
+  const transactions = [
+    {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/5/57/Binance_Logo.png",
+      name: "Binance",
+      date: "Today at 12:34 PM",
+      amount: "0.024",
+    },
+    {
+      image:
+        "https://images.crunchbase.com/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/zl5oeeeuqaqhfyhlifl5",
+      name: "FTX On Ramp",
+      date: "Today at 12:45 PM",
+      amount: "4.245",
+    },
+    {
+      image: "https://static.tvtropes.org/pmwiki/pub/images/ss_glenn.png",
+      name: "Glenn Sturgis OnlyFans",
+      date: "Today at 1:20 PM",
+      amount: "0.010",
+    },
+    {
+      image:
+        "https://i.guim.co.uk/img/media/b0e3074fc9a0b27623a4533872aed1b89e0c73fa/0_53_8192_4915/master/8192.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=709cf73b9b07f55bf30de4309ba35325",
+      name: "CZ Bail Money",
+      date: "Today at 3:34 PM",
+      amount: "25",
+    },
+    {
+      image:
+        "https://pbs.twimg.com/profile_images/1551863971703046146/fl3RyAXc_400x400.jpg",
+      name: "DappRadar Subscription",
+      date: "Today at 4:15 PM",
+      amount: "0.0001",
+    },
+  ];
 
-export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <Head>
+        <title>SplitMonies</title>
+        <meta name="title" content="SplitMonies" />
+      </Head>
+      <div className="p-5 pb-20">
+        <div className="mt-5">
+          <div className="text-gray-900 text-lg font-medium text-center">
+            Available Balance
+          </div>
+          <div className="text-gra-600 text-3xl font-black text-center">
+            23.6422 ETH
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* Menu start */}
+      <div className="grid grid-cols-4 px-1 py-4 bg-white rounded-xl border border-gray-200 shadow-md">
+        <Link href="/top-up" className="text-center space-y-1">
+          <div className="flex justify-center">
+            <CreditCardIcon className="h-8 w-8 text-gray-600" />
+          </div>
+          <div className="text-gray-900 text-xs">Top Up</div>
+        </Link>
+        <Link href="/top-up" className="text-center space-y-1">
+          <div className="flex justify-center">
+            <ArrowsRightLeftIcon className="h-8 w-8 text-gray-600" />
+          </div>
+          <div className="text-gray-900 text-xs">Transfer</div>
+        </Link>
+        <Link href="/top-up" className="text-center space-y-1">
+          <div className="flex justify-center">
+            <ClockIcon className="h-8 w-8 text-gray-600" />
+          </div>
+          <div className="text-gray-900 text-xs">Transactions</div>
+        </Link>
+        <Link href="/top-up" className="text-center space-y-1">
+          <div className="flex justify-center">
+            <BanknotesIcon className="h-8 w-8 text-gray-600" />
+          </div>
+          <div className="text-gray-900 text-xs">Withdraw</div>
+        </Link>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* Menu end */}
+      {/* Wallet address badge start */}
+      <div className="mt-5 flex justify-center">
+        <span className="mx-auto inline-flex items-center gap-x-0.5 rounded-full bg-white px-4 py-1 text-base font-medium text-gray-900 ring-1 ring-inset ring-gray-200">
+          {superShortenAddress(address)}
+          <span className="sr-only">Copy</span>
+          <DocumentDuplicateIcon className="ml-8 h-4 w-4 text-gray-400" />
+        </span>
       </div>
-    </main>
+      {/* Wallet address badge end */}
+      {/* Recent transactions start */}
+      <div className="mt-8 px-5">
+        <div className="text-2xl font-extrabold">Recent Transactions</div>
+        <div className="mt-3">
+          {transactions.map((transaction, index) => (
+            <div
+              key={index}
+              className="py-2.5 flex items-center justify-between"
+            >
+              <div className="flex items-center">
+                <div>
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  <img
+                    className="inline-block h-14 w-14 rounded-full"
+                    src={transaction.image}
+                    alt=""
+                  />
+                </div>
+                <div className="ml-3">
+                  <p className="text-lg font-bold text-gray-900 leading-none">
+                    {transaction.name}
+                  </p>
+                  <p className="mt-2 text-sm font-medium text-gray-600 leading-none">
+                    {transaction.date}
+                  </p>
+                </div>
+              </div>
+              <div className="text-sm text-gray-600 font-bold">
+                {transaction.amount} ETH
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* Recent transactions end */}
+    </>
   );
 }
